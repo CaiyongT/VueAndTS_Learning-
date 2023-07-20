dea464e6710101c8f2911499d591042cf36b8db4
CaiYong_Tuo <1013230638@qq.com>
2023-07-20 21:55:32 +0800

    ci(update0720): entry level study done 0720

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,78 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1> hello, {{ msg }} </h1>
+    <input type="text" v-model="val" @keypress.enter="addTodo">
+    <button @click="addTodo">增加</button>
+    <button @click="clearTodo" v-if="doneCount!=0">清除</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.done" >
+        {{ todo.title }}
+      </li>
+      <div>
+        <input type="checkbox" v-model="allDone">全选
+      </div>
+      <div>
+        <!--强行在DIV中使用JS代码可以实现，但是丑，而且不好管理-->
+        共计事件：{{ todos.length }}
+        已完成：{{ doneCount }}
+        未完成：{{ todoCount }}
+      </div>
+    </ul>
+    
+  </div> 
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  data(){
+    return {
+      msg: 'Tuo',
+      todos:localStorage.getItem('todos')?JSON.parse(localStorage.getItem('todos')):
+      [{title:"吃早饭",done:true},
+      {title:"喝咖啡",done:true},
+      {title:"记单词",done:true}],
+    }
+  },watch:{
+      todos:{
+        handler(){
+          localStorage.setItem('todos',JSON.stringify(this.todos))
+        },
+        deep:true,
+        immediate:true
+      }
+  },
+  computed:{
+    doneCount(){
+      return this.todos.filter(v=>v.done==true).length
+    },
+    todoCount(){
+      return  this.todos.filter(v=>v.done==false).length
+    },
+    allDone:{
+        get(){
+          return  this.doneCount === this.todos.length
+        },
+        set(value){
+          this.todos.forEach(todo => {
+            todo.done = value;
+          });
+        }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.val !== '') {
+        this.todos.push({title:this.val,done:false})
+        this.val = '';
+      }
+    },clearTodo(){
+      this.todos = this.todos.filter(v=>v.done===false)
+    }
   }
+})
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 </style>
